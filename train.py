ad226ad48fcb55b24e0a9d5ed95ae3de43459e06
Ashley Toney <ashleytoney@Ashleys-MacBook-Pro.local>
2021-07-05 14:16:12 -0500

    Import training file

---
+++ train.py
@@ -0,0 +1,182 @@
+'''
+    The following code is adapted from https://github.com/shervinmin/DeepCovid.git
+    and https://github.com/amtoney524/cs598-team1149-final-project
+
+    This is the base PyTorch code which trains a CNN to the presence of COVID-19
+    in chest X-ray images.
+
+    This code will be adapated to run in both the Distributed Data Parallel
+    and Horovod frameworks to explore the effects of distributed frameworks
+    on training speed and resource utilization.
+'''
+
+import copy
+import os
+import time
+
+import torch
+import torch.nn as nn
+from torch import optim
+from torch.optim import lr_scheduler
+
+import torchvision
+from torchvision import datasets
+from torchvision import transforms
+
+# specify image transformations / normalizations for data loader to apply
+
+start_time = time.time()
+
+data_transforms = {
+    'train': transforms.Compose([
+        transforms.Resize(224),
+        transforms.RandomResizedCrop(224),
+        #transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+    'val': transforms.Compose([
+        transforms.Resize(224),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+}
+
+# load images, classes into dataloader and apply transformations
+
+data_dir = "./Data"
+batch_size = 128
+num_workers = 0
+
+image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
+                                          data_transforms[x])
+                  for x in ['train', 'val']}
+dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=batch_size,
+                                             shuffle=True, num_workers=num_workers)
+              for x in ['train', 'val']}
+dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
+class_names = image_datasets['train'].classes  ## 0: child, and 1: nonchild
+
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+def train_model(model, criterion, optimizer, scheduler, batch_szie, num_epochs=20):
+    since = time.time()
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+    train_acc = list()
+    valid_acc = list()
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch + 1, num_epochs))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                scheduler.step()
+                model.train()  # Set model to training mode
+            else:
+                model.eval()  # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+            running_prec = 0.0
+            running_rec = 0.0
+            running_f1 = 0.0
+
+            # Iterate over data.
+            cur_batch_ind = 0
+            for inputs, labels in dataloaders[phase]:
+                # print(cur_batch_ind,"batch inputs shape:", inputs.shape)
+                # print(cur_batch_ind,"batch label shape:", labels.shape)
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+                cur_acc = torch.sum(preds == labels.data).double() / batch_szie
+                cur_batch_ind += 1
+                print("\npreds:", preds)
+                print("label:", labels.data)
+                print("%d-th epoch, %d-th batch (size=%d), %s acc= %.3f \n" % (
+                epoch + 1, cur_batch_ind, len(labels), phase, cur_acc))
+
+                if phase == 'train':
+                    train_acc.append(cur_acc)
+                else:
+                    valid_acc.append(cur_acc)
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects.double() / dataset_sizes[phase]
+
+            print('{} Loss: {:.4f} Acc: {:.4f} \n\n'.format(
+                phase, epoch_loss, epoch_acc))
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_epoch = epoch
+                best_model_wts = copy.deepcopy(model.state_dict())
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc= %.3f at Epoch: %d' % (best_acc, best_epoch))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model, train_acc, valid_acc
+
+epochs = 20
+learning_rate = 0.0001
+momentum = 0.9
+
+#### load model
+model_conv = torchvision.models.resnet18(pretrained=True)
+for param in model_conv.parameters():
+    param.requires_grad = False
+
+# Parameters of newly constructed modules have requires_grad=True by default
+num_ftrs = model_conv.fc.in_features
+model_conv.fc = nn.Linear(num_ftrs, 2)
+
+
+model_conv = model_conv.to(device)
+criterion = nn.CrossEntropyLoss()
+
+# Observe that only parameters of final layer are being optimized as
+# opoosed to before.
+optimizer_conv = optim.SGD(model_conv.fc.parameters(), lr=learning_rate, momentum=momentum)
+
+# Decay LR by a factor of 0.1 every 7 epochs
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=7, gamma=0.1)
+
+
+model_conv, train_acc, valid_acc = train_model(model_conv, criterion, optimizer_conv,
+                                               exp_lr_scheduler, batch_size, num_epochs=epochs)
+model_conv.eval()
+torch.save(model_conv, './covid_resnet18_epoch%d.pt' %epochs )
+
+end_time= time.time()
+print("total_time tranfer learning=", end_time - start_time)
