711c7385f4945c86f0e8129bd45adbfe5ba5de97
Jon-LaFlamme <jonlaflamme7@gmail.com>
2021-07-16 16:53:52 -0400

    comments for cluster and efs config

--- testwriter.py
+++ testwriter.py
@@ -1,7 +1,40 @@
-if __name__ == '__main__':
-    main()
+"""
+https://stackoverflow.com/questions/52558062/auto-mounting-efs-on-ec2-instance
+
+EC2 configs ->
+    Region: US-east-1
+    Placement group:
+            - Disable for development
+            - Enable ddp (type: cluster) for production
+    IAM role: Ashley-Jon-Nodirbek-Ec2-S3
+    Tenancy: 
+            - Shared for development
+            - Dedicated for testing
+    File Systems:
+            - fs-25d7a491 | ddp
+    Edit Installs script to include ML packages
+    Security Groups: Enable SSH from "This IP"
+  
+- mount point: /mnt/efs/fs1
+
+
+EFS Security Group: sg-d4cc56cf
+
+$ mkdir ~/ddp
+$ sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-25d7a491.efs.us-east-1.amazonaws.com:/ ~/ddp
+$ sudo mount -t efs [fs-XXXXXXXX]:/ /path/to/mount/dir
+
+"""
+
+
+
 
 def main():
     with open('/test-logs/testlog.txt', 'w') as f:
         for i in range(len(100)):
             f.write(f'log: {i}')
+
+
+
+if __name__ == '__main__':
+    main()
